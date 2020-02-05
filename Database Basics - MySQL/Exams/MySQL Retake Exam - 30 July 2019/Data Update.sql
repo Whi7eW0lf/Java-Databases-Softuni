f64c4ed6b0f0ac008780092200d1a351f2554db9
Slavi <slaveizlatarev@gmail.com>
2020-02-05 19:21:51 +0200

    3.	Data Update

---
+++ Database Basics - MySQL/Exams/MySQL Retake Exam - 30 July 2019/Data Update.sql
@@ -0,0 +1,10 @@
+UPDATE `comments`
+   SET `comment` =
+       CASE
+       WHEN id%2 = 0 THEN 'Very good article.'
+       WHEN id%3 = 0 THEN 'This is interesting.'
+       WHEN id%5 = 0 THEN 'I definitely will read the article again.'
+       WHEN id%7 = 0 THEN 'The universe is such an amazing thing.'
+           else comment
+       END
+WHERE id BETWEEN 1 AND 15;
